f71992bed854967d9344718ef0501e8462a4cd24
unknown <mkaczmarczykszkola@gmail.com>
2025-09-30 15:50:51 +0200

    Updated sql code

--- database.sql
+++ database.sql
@@ -1,3 +1,13 @@
+-- Drop the database if it already exists to start with a clean slate
+DROP DATABASE IF EXISTS logowanie;
+
+-- Create the new database
+CREATE DATABASE logowanie;
+
+-- Select the newly created database to use it for the subsequent commands
+USE logowanie;
+
+-- Create the 'users' table
 CREATE TABLE `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `username` VARCHAR(50) NOT NULL UNIQUE,
